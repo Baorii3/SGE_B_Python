9af438ef8d4bff71cffba6c32507a9ed9f92e23b
Baorii3 <baorii310@gmail.com>
2024-11-28 11:41:27 +0100

    borrar tot de main

--- ex1.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Primer numero: "))
-b = int(input("Segundo numero: "))
-if a > b:
-    print(f"{a} es mas grande que {b}")
-elif a < b:
-    print(f"{b} es mas grande que {a}")
-else:
-    print(f"{b} es  igual que {a}")
